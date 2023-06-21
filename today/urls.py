cd35d1936cbefebb433e29dc684cc745bb9b616f
rivka-yuliia <juliacreadora@gmail.com>
2023-06-21 17:16:03 +0300

    mark done view

--- today/urls.py
+++ today/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TodayView, undo_task
+from .views import TodayView, undo_task, mark_done
 
 app_name = 'today'
 
 urlpatterns = [
     path('', TodayView.as_view(), name='today_tasks'),
-    path('undo/', undo_task, name='undo'),
+    path('done/<int:task_id>/', mark_done, name='done'),
+    path('undo/<int:task_id>/', undo_task, name='undo'),
 ]
--- today/views.py
+++ today/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from .models import TaskModel
 
 
 class TodayView(View):
     def get(self, request):
-        return render(request, 'today/today.html', {'title': 'TODO LIST - Today'})
+        tasks = TaskModel.objects.filter(is_done=False)
+        completed = TaskModel.objects.filter(is_done=True)
+        return render(request, 'today/today.html', {
+            'title': 'TODO LIST - Today',
+            'tasks': tasks,
+            'completed': completed
+        })
 
 
-def undo_task(request):
+def mark_done(request, task_id):
+    task = TaskModel.objects.get(pk=task_id)
+    task.is_done = True
+    task.save()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def undo_task(request, task_id):
     print('undo')
     return redirect('today:today_tasks')
