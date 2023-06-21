6ee519d3831edd13ed630fdc7784422ad617b774
rivka-yuliia <juliacreadora@gmail.com>
2023-06-21 17:43:24 +0300

    rename task model

--- today/models.py
+++ today/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class TaskModel(models.Model):
+class Task(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- today/views.py
+++ today/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .models import TaskModel
+from .models import Task
 
 
 class TodayView(View):
     def get(self, request):
-        tasks = TaskModel.objects.filter(is_done=False)
-        completed = TaskModel.objects.filter(is_done=True)
+        tasks = Task.objects.filter(is_done=False)
+        completed = Task.objects.filter(is_done=True)
         return render(request, 'today/today.html', {
             'title': 'TODO LIST - Today',
             'tasks': tasks,
@@ -15,26 +15,26 @@ class TodayView(View):
 
     def post(self, request):
         task_name = request.POST['add_task']
-        new_task = TaskModel(name=task_name)
+        new_task = Task(name=task_name)
         new_task.save()
         return redirect(request.META['HTTP_REFERER'])
 
 
 def mark_done(request, task_id):
-    task = TaskModel.objects.get(pk=task_id)
+    task = Task.objects.get(pk=task_id)
     task.is_done = True
     task.save()
     return redirect(request.META['HTTP_REFERER'])
 
 
 def delete_task(request, task_id):
-    task = TaskModel.objects.get(pk=task_id)
+    task = Task.objects.get(pk=task_id)
     task.delete()
     return redirect(request.META['HTTP_REFERER'])
 
 
 def undo_task(request, task_id):
-    task = TaskModel.objects.get(pk=task_id)
+    task = Task.objects.get(pk=task_id)
     task.is_done = False
     task.save()
     return redirect('today:today_tasks')
