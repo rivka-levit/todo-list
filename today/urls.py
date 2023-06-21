cc30e116b3a6c5a183dc1df2e4745f4b200b0cfe
rivka-yuliia <juliacreadora@gmail.com>
2023-06-21 16:54:13 +0300

    undo view, url

--- today/urls.py
+++ today/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodayView
+from .views import TodayView, undo_task
 
 app_name = 'today'
 
 urlpatterns = [
     path('', TodayView.as_view(), name='today_tasks'),
+    path('undo/', undo_task, name='undo'),
 ]
--- today/views.py
+++ today/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 
 class TodayView(View):
     def get(self, request):
         return render(request, 'today/today.html', {'title': 'TODO LIST - Today'})
+
+
+def undo_task(request):
+    print('undo')
+    return redirect('today:today_tasks')
