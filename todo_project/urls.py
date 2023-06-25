5e80eb148b7da7857176dbdaeb20efafd62d461d
rivka-yuliia <juliacreadora@gmail.com>
2023-06-25 19:12:53 +0300

    index

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from todo_project.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo.urls', namespace='todo'))
+    path('', include('todo.urls', namespace='todo')),
+    path('', IndexView.as_view(), name='index'),
 ]
